56a5c680d0e3fe03a1fc312a6a7d0e6362575feb
Bart Slinger <bartslinger@gmail.com>
2019-02-07 15:22:49 +0100

    airmap node get pilot id

--- src/airmap_node/airmap_node.c
+++ src/airmap_node/airmap_node.c
@@ -31,6 +31,8 @@ typedef struct airmap_node_t
 {
 	const char* name;
 	airmap_state_t state;
+	char bearer_token[300];
+	char pilot_id[100];
 	int req_fp_fd;
 } airmap_node_t;
 
@@ -57,29 +59,12 @@ size_t write_data_callback(void *ptr, size_t size, size_t nmemb, struct url_data
 	memcpy((data->data + index), ptr, n);
 	data->data[data->size] = '\0';
 
-	// Try some JSON parsing
-	cJSON *json = cJSON_Parse(data->data);
-
-	if (json == NULL) {
-		fprintf(stderr, "json parse failed");
-	}
-
-	cJSON *access_token = cJSON_GetObjectItemCaseSensitive(json, "access_token");
-	if (cJSON_IsString(access_token) && access_token->valuestring != NULL) {
-		printf("parsed access_token, it is: %s\n", access_token->valuestring);
-	}
-
 	return n;
 }
 
 int airmap_connect(airmap_node_t *node)
 {
-	// Get the bearer-token
-	CURL *curl = curl_easy_init();
-	if (!curl) {
-		printf("fail curl easy init");
-		return 1;
-	}
+	int ret = 1;
 
 	struct url_data data;
 	data.size = 0;
@@ -87,15 +72,23 @@ int airmap_connect(airmap_node_t *node)
 
 	if (data.data == NULL) {
 		fprintf(stderr, "malloc failed");
+		return ret;
 	}
-
 	data.data[0] = '\0';
+
+	CURL *curl = curl_easy_init();
+	if (!curl) {
+		printf("fail curl easy init");
+		free(data.data);
+		return ret;
+	}
 
 	char post_data[] = "grant_type=password"
 			    "&client_id=" AIRMAP_CLIENT_ID
 			    "&connection=Username-Password-Authentication"
 			    "&username=" AIRMAP_USERNAME
 			    "&password=" AIRMAP_PASSWORD
+			    "&scope=openid offline_access"
 			    "&device=" AIRMAP_DEVICE_ID;
 
 	CURLcode res;
@@ -108,16 +101,126 @@ int airmap_connect(airmap_node_t *node)
 
 	res = curl_easy_perform(curl);
 	if (res != CURLE_OK) {
-		printf("return code: %i\n", res);
+		fprintf(stderr, "curl return code: %i\n", res);
+		free(data.data);
+		return ret;
 	}
 
 	curl_easy_cleanup(curl);
 
-	printf("Got:\n%s\n\n", data.data);
+	// JSON parsing to get the token out
+	cJSON *json = cJSON_Parse(data.data);
+
+	if (json == NULL) {
+		fprintf(stderr, "json parse failed");
+		free(data.data);
+		return ret;
+	}
+
+	cJSON *id_token = cJSON_GetObjectItemCaseSensitive(json, "id_token");
+	if (cJSON_IsString(id_token) && id_token->valuestring != NULL) {
+		size_t len = strlen(id_token->valuestring);
+		printf("bearertokensize = %lu\n", sizeof(node->bearer_token));
+		if ((len + 1) > sizeof(node->bearer_token)) {
+			fprintf(stderr, "id_token too large for storage\n");
+		} else {
+			// storage is sufficiently large for the token
+			memset(node->bearer_token, 0, sizeof(node->bearer_token));
+			memcpy(node->bearer_token, id_token->valuestring, len);
+			ret = 0;
+		}
+	}
+
+	cJSON_Delete(json);
 
 	free(data.data);
 
-	return res;
+	return ret;
+}
+
+int airmap_get_pilot_id(airmap_node_t *node)
+{
+	int ret = 1;
+
+	struct url_data data;
+	data.size = 0;
+	data.data = malloc(4096);
+
+	if (data.data == NULL) {
+		fprintf(stderr, "malloc failed");
+		return ret;
+	}
+	data.data[0] = '\0';
+
+	struct curl_slist *list = NULL;
+
+	CURL *curl = curl_easy_init();
+	if (!curl) {
+		printf("fail curl easy init");
+		free(data.data);
+		return ret;
+	}
+
+	char token_header[350];
+	char api_header[500];
+	sprintf(token_header, "Authorization: Bearer %s", node->bearer_token);
+	sprintf(api_header, "X-API-Key: %s", AIRMAP_API_KEY);
+	printf("%s\n", token_header);
+	printf("%s\n", api_header);
+
+	list = curl_slist_append(list, token_header);
+	list = curl_slist_append(list, api_header);
+
+	CURLcode res;
+
+	curl_easy_setopt(curl, CURLOPT_URL, "https://" AIRMAP_HOST "/pilot/v2/profile");
+	curl_easy_setopt(curl, CURLOPT_HTTPGET, 1);
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+
+	res = curl_easy_perform(curl);
+	curl_slist_free_all(list);
+
+	if (res != CURLE_OK) {
+		fprintf(stderr, "curl return code: %i\n", res);
+		free(data.data);
+		return ret;
+	}
+
+	curl_easy_cleanup(curl);
+
+	// JSON parsing to get the token out
+	cJSON *json = cJSON_Parse(data.data);
+
+	if (json == NULL) {
+		fprintf(stderr, "json parse failed");
+		free(data.data);
+		return ret;
+	}
+
+	cJSON *user_data = cJSON_GetObjectItemCaseSensitive(json, "data");
+	if (cJSON_IsObject(user_data)) {
+		cJSON *pilot_id = cJSON_GetObjectItemCaseSensitive(user_data, "id");
+		if (cJSON_IsString(pilot_id) && pilot_id->valuestring != NULL) {
+			printf("pilot_id: %s\n", pilot_id->valuestring);
+			ssize_t len = strlen(pilot_id->valuestring);
+			if ((len + 1) > sizeof(node->pilot_id)) {
+				fprintf(stderr, "pilot_id too large for storage\n");
+			} else {
+				// copy pilot_id
+				memset(node->pilot_id, 0, sizeof(node->pilot_id));
+				memcpy(node->pilot_id, pilot_id->valuestring, len);
+				ret = 0;
+			}
+		}
+	}
+
+	cJSON_Delete(json);
+
+	free(data.data);
+
+	return ret;
 }
 
 int main(int argc, char **argv)
@@ -132,7 +235,25 @@ int main(int argc, char **argv)
 	int rv;
 	printf("Starting\n");
 
-	airmap_connect(&node);
+	if ((rv = airmap_connect(&node)) == 0) {
+		node.state = AIRMAP_STATE_CONNECTED;
+
+	} else {
+		fprintf(stderr, "airmap_connect failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((rv = airmap_get_pilot_id(&node)) == 0) {
+		node.state = AIRMAP_STATE_GOT_PILOT_ID;
+
+	} else {
+		fprintf(stderr, "airmap_get_pilot_id failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (node.state == AIRMAP_STATE_GOT_PILOT_ID) {
+		printf("\n\nGOT PILOT ID: %s\n", node.pilot_id);
+	}
 
 	exit(EXIT_SUCCESS);
 
