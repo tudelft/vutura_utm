b121b4bf157e0d67d391b76fa48a416de9817e71
Bart Slinger <bartslinger@users.noreply.github.com>
2019-02-23 01:51:03 +0100

    Update airmap telemetry server and port

--- src/airmap_node/airmap_config.h
+++ src/airmap_node/airmap_config.h
@@ -5,8 +5,8 @@
 #define AIRMAP_SSO_HOST "sso.airmap.io"
 #define AIRMAP_SSO_PORT 443
 
-#define AIRMAP_TELEM_HOST "api-udp-telemetry.airmap.com"
-#define AIRMAP_TELEM_PORT 16000
+#define AIRMAP_TELEM_HOST "telemetry.airmap.com"
+#define AIRMAP_TELEM_PORT 16060
 
 #define AIRMAP_TRAFF_HOST "mqtt-prod.airmap.io"
 #define AIRMAP_TRAFF_PORT 8883
