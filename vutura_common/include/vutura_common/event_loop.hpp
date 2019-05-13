f50b92e0a70f64f89f743486446f4205dbde0478
Bart Slinger <bartslinger@gmail.com>
2019-05-13 13:52:08 +0200

    allow interrupting the event loop with ctrl+c

--- vutura_common/include/vutura_common/event_loop.hpp
+++ vutura_common/include/vutura_common/event_loop.hpp
@@ -2,16 +2,22 @@
 
 #include "event_source.hpp"
 
+typedef void (*sigterm_event_handler_t) (int);
 
 class EventLoop
 {
 public:
 	EventLoop();
 	int add(EventSource &source);
+	int add_sigterm_event_handler(sigterm_event_handler_t event_handler);
 	void start();
 	void stop();
+
+	static void sigterm_handler(int param);
+
+
 private:
 	std::vector<EventSource*> _event_sources;
+	std::vector<sigterm_event_handler_t> _sigterm_event_handlers;
 	bool _should_exit;
 };
-
--- vutura_common/src/event_loop.cpp
+++ vutura_common/src/event_loop.cpp
@@ -1,15 +1,33 @@
+#include <signal.h>
 #include <sys/stat.h>
 #include "event_loop.hpp"
+
+namespace event_loop {
+	EventLoop* object = nullptr;
+	bool sigterm_received = false;
+	int sigterm_param = 0;
+}
 
 EventLoop::EventLoop() :
 	_should_exit(false)
 {
-
+	if (event_loop::object != nullptr) {
+		std::cerr << "Event loop already exists" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	event_loop::object = this;
+	signal(SIGINT, sigterm_handler);
+	signal(SIGTERM, sigterm_handler);
 }
 
 int EventLoop::add(EventSource &source)
 {
 	_event_sources.push_back(&source);
+}
+
+int EventLoop::add_sigterm_event_handler(sigterm_event_handler_t event_handler)
+{
+	_sigterm_event_handlers.push_back(event_handler);
 }
 
 void EventLoop::start()
@@ -24,7 +42,17 @@ void EventLoop::start()
 		fds[i].revents = 0;
 	}
 
-	while (!_should_exit) {
+	while (true) {
+		if (event_loop::sigterm_received) {
+			// send sigterm event to callbacks
+			for (ssize_t i = 0; i < _sigterm_event_handlers.size(); i++) {
+				//std::cout << "sigterm event handler calling" << std::endl;
+				_sigterm_event_handlers.at(i)(event_loop::sigterm_param);
+			}
+
+			// break the loop, this ends the program
+			break;
+		}
 		int ret = poll(fds, len, 2000);
 
 		if (ret == 0) {
@@ -49,3 +77,13 @@ void EventLoop::stop()
 	_should_exit = true;
 }
 
+void EventLoop::sigterm_handler(int param)
+{
+	if (event_loop::sigterm_received) {
+		std::cerr << "Already received a sigterm signal" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	std::cout << "[event_loop] SIGTERM/SIGINT" << std::endl;
+	event_loop::sigterm_received = true;
+	event_loop::sigterm_param = param;
+}
