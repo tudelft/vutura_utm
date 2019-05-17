afa66f79207a2ad94e348acf304ea3169d6b9fa5
Bart Slinger <bartslinger@gmail.com>
2019-05-17 14:42:36 +0200

    web interface added some buttons

--- web_interface/src/App.vue
+++ web_interface/src/App.vue
@@ -1,30 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/aed.png" width="200">
+    <button v-promise-btn="{ promise: dataPromise }" @click="getData('param')">Request AED</button>
+    <button v-promise-btn @click="startFlight">Start Flight</button>
     <div class="column" align="center">
       <drag-verify :width="killSlider.width"
-			 :height="killSlider.height" 
-			 :text="killSlider.text" 
-			 :success-text="killSlider.successText" 
-			 :background="killSlider.background" 
-			 :progress-bar-bg="killSlider.progressBarBg" 
-			 :completed-bg="killSlider.completedBg" 
-			 :handler-bg="killSlider.handlerBg" 
-			 :handler-icon="killSlider.handlerIcon" 
-			 :text-size="killSlider.textSize" 
-			 :success-icon="killSlider.successIcon" 
-			 v-on:passcallback="killHandler"></drag-verify>
-      </drag-verify>
+         :height="killSlider.height" 
+         :text="killSlider.text"
+         :success-text="killSlider.successText" 
+         :background="killSlider.background" 
+         :progress-bar-bg="killSlider.progressBarBg" 
+         :completed-bg="killSlider.completedBg" 
+         :handler-bg="killSlider.handlerBg" 
+         :handler-icon="killSlider.handlerIcon" 
+         :text-size="killSlider.textSize" 
+         :success-icon="killSlider.successIcon" 
+         v-on:passcallback="killHandler"></drag-verify>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import dragVerify from 'vue-drag-verify'
+import VuePromiseBtn from 'vue-promise-btn'
+import 'vue-promise-btn/dist/vue-promise-btn.css'
+
+Vue.use(VuePromiseBtn)
 export default {
   name: 'app',
   components: {
-    dragVerify
+    dragVerify,
+    VuePromiseBtn
   },
   data() {
     return {
@@ -36,12 +43,28 @@ export default {
         textSize: "40px",
         progressBarBg: "#ff0000",
         completedBg: "#ff0000",
-      }
+      },
+      dataPromise: null
     }
   },
   methods: {
     killHandler() {
       console.log("KILL SIGNAL");
+    },
+    dummyAsyncAction () {
+      console.log("async");
+      return new Promise((resolve, reject) => setTimeout(resolve, 2000))
+    },
+    isLoading() {
+      console.log("isLoading");
+    },
+    getData() {
+      console.log("getData");
+      this.dataPromise = this.dummyAsyncAction();
+      console.log("door");
+    },
+    startFlight() {
+      console.log("Start flight");
     }
   }
 }
@@ -56,4 +79,44 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+button,
+input[type="submit"] {
+  display: inline-block;
+  vertical-align: middle;
+  background-color: #3b98ab;
+  border: 1px solid #3b98ab;
+  border-radius: 14px;
+  padding: 18px 22px;
+  font: 34px/40px Arial, Helverica, sans-serif;
+  color: #fff;
+  margin: 0;
+  text-decoration: none;
+  text-align: center;
+  cursor: pointer;
+  -webkit-transition: background-color 0.1s linear, color 0.1s linear, width 0.2s ease;
+  transition: background-color 0.1s linear, color 0.1s linear, width 0.2s ease;
+}
+
+button[disabled],
+input[type="submit"][disabled] {
+  background: #a0d4de;
+  border-color: #a0d4de;
+  cursor: default;
+}
+
+button .promise-btn__spinner-wrapper {
+  display: inline-block;
+  margin: -8px 0;
+}
+
+button.hide-loader .hidden {
+  display: none;
+}
+
+.site-footer {
+  display: flex;
+  justify-content: space-between;
+}
+
 </style>
