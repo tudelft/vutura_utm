59e362192444c4676eb26366d178736feafb4a16
Bart Slinger <bartslinger@gmail.com>
2019-05-27 10:15:12 +0200

    unifly socketcluster python traffic publisher

--- python/socketcluster.py
+++ python/socketcluster.py
@@ -3,9 +3,15 @@
 from socketclusterclient import Socketcluster
 
 import logging
+import nanomsg
+import json
+import vutura_common_pb2
+import dateutil.parser
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
+traffic_pub = nanomsg.Socket(nanomsg.PUB)
+traffic_pub.bind('tcp://127.0.0.1:8340'.encode('utf-8'))
 
 def onconnect(socket):
     logging.info("on connect got called")
@@ -31,7 +37,28 @@ def onAuthentication(socket, isauthenticated):
     socket.onchannel('adsb', onTraffic)
 
 def onTraffic(key, object):
-    print("Got data\n\n\n", object, "\n\nfrom\n", key)
+    try:
+        d = json.dumps(object)
+        j = json.loads(d)
+        for item in j["data"]:
+            traffic_message = vutura_common_pb2.TrafficInfo()
+            traffic_message.unique_id = item["movement"]
+            traffic_message.aircraft_id = item["identification"]
+            recorded_time = dateutil.parser.parse(item["timestamp"])
+            traffic_message.timestamp = round(recorded_time.timestamp() * 1000)
+            traffic_message.recorded_time = round(recorded_time.timestamp() * 1000)
+            traffic_message.lat = round(item["location"]["latitude"] * 1e7)
+            traffic_message.lon = round(item["location"]["longitude"] * 1e7)
+            traffic_message.alt = round(item["altitude"]["value"] * 1e3)
+            traffic_message.groundspeed = 0
+            traffic_message.heading = 0
+            traffic_pub.send(traffic_message.SerializeToString())
+            if (item["identification"] == "flight|G8ywkEn8JZ7N6hg2nywOXg8voKulqoa72wDYZyOHWBPanvQNDZk5"):
+                print(json.dumps(item, indent=4))
+    except:
+        print("error parsing json")
+
+    #print("Got data\n\n\n", object, "\n\nfrom\n", key)
 
 def onSubAck(channel, error, object):
     if error is '':
@@ -41,7 +68,7 @@ def onSubAck(channel, error, object):
     
     
 if __name__ == "__main__":
-    socket = Socketcluster.socket("wss://vutura.unifly.tech/socketcluster/")
+    socket = Socketcluster.socket("wss://vutura.unifly.tech:443/socketcluster/")
     socket.setBasicListener(onconnect, ondisconnect, onConnectError)
     socket.setAuthenticationListener(onSetAuthentication, onAuthentication)
 
