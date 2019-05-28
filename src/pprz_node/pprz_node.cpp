acbad1189d6ba8616df34bb92017393230349dda
Bart Slinger <bartslinger@gmail.com>
2019-05-28 21:59:29 +0200

    communicate with pprz flightplan for utm status/requests

--- src/pprz_node/pprz_node.cpp
+++ src/pprz_node/pprz_node.cpp
@@ -11,8 +11,10 @@ PaparazziNode::PaparazziNode(int instance) :
 	_pprz_comm(this),
 	_avoidance_sub(this),
 	_position_publisher(socket_name(SOCK_PUBSUB_GPS_POSITION, instance)),
+	_utm_interface(this),
 	_utm_status_sub(this),
-	_utm_status("")
+	_utm_status(""),
+	_utm_sp_req(this, socket_name(SOCK_REQREP_UTMSP_COMMAND, instance))
 {
 
 }
@@ -30,13 +32,18 @@ void PaparazziNode::init()
 	_avoidance_sub.set_receive_callback(std::bind(&PaparazziNode::avoidance_command_callback, this, _1));
 	_avoidance_sub.subscribe(socket_name(SOCK_REQREP_AVOIDANCE_COMMAND, _instance));
 
+	_utm_interface.set_receive_callback(std::bind(&PaparazziNode::utm_interface_callback, this, _1));
+	_utm_interface.connect(PPRZ_IP, PPRZ_PORT+1, PPRZ_PORT+101);
+
 	_utm_status_sub.set_receive_callback(std::bind(&PaparazziNode::utm_status_callback, this, _1));
 	_utm_status_sub.subscribe(socket_name(SOCK_PUBSUB_UTM_STATUS_UPDATE, _instance));
+
+	_utm_sp_req.set_receive_callback(std::bind(&PaparazziNode::utm_sp_receive_callback, this, _1));
 }
 
 void PaparazziNode::pprz_callback(std::string packet)
 {
-	std::cout << "Got UDP data with size: " << packet.length() << std::endl;
+//	std::cout << sizeof(PaparazziToVuturaMsg) << " Got UDP data with size: " << packet.length() << std::endl;
 
 	if (packet.length() == sizeof(PaparazziToVuturaMsg)) {
 		memcpy((char*)&_gpos_msg, packet.c_str(), packet.length());
@@ -52,6 +59,7 @@ void PaparazziNode::pprz_callback(std::string packet)
 		msg.set_target_wp(_gpos_msg.target_wp);
 		std::string gps_message = msg.SerializeAsString();
 		_position_publisher.publish(gps_message);
+
 	}
 }
 
@@ -65,23 +73,81 @@ void PaparazziNode::avoidance_command_callback(std::string message)
 	}
 }
 
+void PaparazziNode::utm_interface_callback(std::string packet)
+{
+	// handle request, used for request, start and end flight
+	PaparazziToUtmInterfaceMsg msg;
+	if (packet.length() == sizeof(PaparazziToUtmInterfaceMsg)) {
+		memcpy((char*)&msg, packet.c_str(), packet.length());
+
+		std::cout << "Got request: " << std::to_string(msg.utm_request) << std::endl;
+
+		std::string request = "";
+		switch (msg.utm_request) {
+		case UTM_REQUEST_FLIGHT:
+			request = "request_flight";
+			break;
+
+		case UTM_REQUEST_START:
+			request = "start_flight";
+			break;
+
+		case UTM_REQUEST_END:
+			request = "end_flight";
+			break;
+
+		default:
+			break;
+		}
+
+		if (request != "") {
+			_utm_sp_req.request(request);
+		}
+	}
+}
+
 void PaparazziNode::utm_status_callback(std::string status)
 {
 	if (_utm_status != status) {
 		_utm_status = status;
 		std::cout << "UTM status update: " << status << std::endl;
-
-		// Also do something with it.
-		// For example, switch flight plan block to notify user of authorized/rejected flight
-
-		// Some of the possible states are:
-		// "logged in"			Can do the request now, also for rejected
-		// "flight requested"		Waiting for permission
-		// "flight authorized"		Flight authorized
-		// "flight started"		Streaming position
-		// "flight rescinded"		ATC wants you to land
-		// "flight cancelled"		ATC landing request was confirmed
 	}
+	// Also do something with it.
+	// For example, switch flight plan block to notify user of authorized/rejected flight
+	UtmInterfaceToPaparazziMsg msg;
+	msg.utm_state = -1;
+
+	if (status == "logged in") {
+		msg.utm_state = UTM_STATE_LOGGED_IN;
+
+	} else if (status == "flight requested") {
+		msg.utm_state = UTM_STATE_REQUEST_PENDING;
+
+	} else if (status == "flight authorized") {
+		msg.utm_state = UTM_STATE_REQUEST_APPROVED;
+
+	} else if (status == "flight started") {
+		msg.utm_state = UTM_STATE_FLIGHT_STARTED;
+
+	}
+
+	if (msg.utm_state != -1) {
+		std::string packet((char*)&msg, sizeof(msg));
+		_utm_interface.send_packet(packet);
+	}
+
+	// Some of the possible states are:
+	// "logged in"			Can do the request now, also for rejected
+	// "flight requested"		Waiting for permission
+	// "flight authorized"		Flight authorized
+	// "flight started"		Streaming position
+	// "flight rescinded"		ATC wants you to land
+	// "flight cancelled"		ATC landing request was confirmed
+}
+
+void PaparazziNode::utm_sp_receive_callback(std::string reply)
+{
+
 }
 
 int PaparazziNode::handle_avoidance(AvoidanceVelocity avoidance_velocity)
--- src/pprz_node/pprz_node.hpp
+++ src/pprz_node/pprz_node.hpp
@@ -6,8 +6,24 @@
 #include "nng_event_loop/udp_source.hpp"
 #include "nng_event_loop/publisher.hpp"
 #include "nng_event_loop/subscriber.hpp"
+#include "nng_event_loop/requester.hpp"
 
 #include "vutura_common.pb.h"
+
+enum utm_state_t {
+	UTM_STATE_INIT = 0,
+	UTM_STATE_LOGGED_IN,
+	UTM_STATE_REQUEST_PENDING,
+	UTM_STATE_REQUEST_REJECTED,
+	UTM_STATE_REQUEST_APPROVED,
+	UTM_STATE_FLIGHT_STARTED
+};
+
+enum utm_request_t {
+	UTM_REQUEST_FLIGHT,
+	UTM_REQUEST_START,
+	UTM_REQUEST_END
+};
 
 union paparazzi_to_vutura_msg_t {
 	struct {
@@ -36,6 +52,23 @@ union vutura_to_paparazzi_msg_t {
 } __attribute((__packed__));
 typedef union vutura_to_paparazzi_msg_t VuturaToPaparazziMsg;
 
+union paparazzi_to_utm_interface_t {
+	struct {
+		int32_t utm_request;
+	};
+	unsigned char bytes;
+} __attribute((__packed__));
+typedef union paparazzi_to_utm_interface_t PaparazziToUtmInterfaceMsg;
+
+union utm_interface_to_paparazzi_t {
+	struct {
+		int32_t utm_state;
+	};
+	unsigned char bytes;
+} __attribute((__packed__));
+typedef union utm_interface_to_paparazzi_t UtmInterfaceToPaparazziMsg;
+
+
 class PaparazziNode : public EventLoop {
 public:
 	PaparazziNode(int instance);
@@ -44,7 +77,9 @@ public:
 	void init();
 	void pprz_callback(std::string packet);
 	void avoidance_command_callback(std::string message);
+	void utm_interface_callback(std::string packet);
 	void utm_status_callback(std::string status);
+	void utm_sp_receive_callback(std::string reply);
 
 private:
 	int _instance;
@@ -52,9 +87,11 @@ private:
 	UdpSource _pprz_comm;
 	Subscriber _avoidance_sub;
 	Publisher _position_publisher;
+	UdpSource _utm_interface;
 
 	Subscriber _utm_status_sub;
 	std::string _utm_status;
+	Requester _utm_sp_req;
 
 	PaparazziToVuturaMsg _gpos_msg;
 
